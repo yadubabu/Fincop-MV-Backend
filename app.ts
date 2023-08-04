a15ad4407f5e15f7305590b8f0a8675e23fb16a0
yadubabu <100501154+yadubabu@users.noreply.github.com>
2023-08-04 11:39:02 +0530

    Update app.ts

--- app.ts
+++ app.ts
@@ -4,13 +4,16 @@ import {connect} from './dbConnect';
 import mongoose from 'mongoose';
 import {Request,Response,NextFunction} from 'express';
 import userRoute from './routes/userRoute';
+import cors from 'cors';
 import transRoute from './routes/transRoute';
 
 const app=express();
 
 app.use(express.json());
-
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors({
+    origin:'*'
+}))
+// app.use(bodyParser.urlencoded({extended:true}));
 
 
 app.get('/',(req:Request,res:Response)=>{
@@ -22,11 +25,15 @@ mongoose.connect(connect).then(()=>{
     console.log('DB Connected');
     
 });
-app.use('/user',userRoute);
+app.use('/user/getuser',userRoute);
 
 app.use('/user/adduser',userRoute);
-app.use('/user',userRoute);
-app.use('/api',transRoute);
+
+app.use('/user/login',userRoute);
+
+app.use('/api/gettrans',transRoute);
+
+app.use('/api/addtrans',transRoute);
 
 app.listen(5000,()=>{
     console.log('Server is Running on 5000');
