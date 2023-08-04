6ce9b5d1f43094dd919c5820763762366de3ca87
yadubabu <100501154+yadubabu@users.noreply.github.com>
2023-08-04 11:38:19 +0530

    Update userRoute.ts

--- routes/userRoute.ts
+++ routes/userRoute.ts
@@ -1,54 +1,15 @@
 import express from 'express';
-import { Request,Response } from 'express';
 import { createUser } from '../controllers/userController';
 import { getUser } from '../controllers/userController';
-import UserModel from "../models/UserModel";
-import * as bcrypt from 'bcrypt';
-import AccountModel from '../models/AccountModel';
+import { loginuser } from '../controllers/userController';
 
 const router=express.Router();
 
-router.get('/getuser',getUser);
+router.get('/',getUser);
 
-router.post('/login',async(req:Request,res:Response)=>{
-    const {email,pass}=req.body;
-    console.log(email,pass);
-    
-})
+router.post('/',createUser);
 
-router.post('/',async(req:Request,res:Response)=>{
-        const {name,email,password,confirmpassword,pancard,phone}=req.body;
-             console.log(req.body);
-          let hashPassword=await bcrypt.hash(password,12);
-          if(!email || password===confirmpassword){
-          }
-            try{
-            
-            const newUser=new UserModel({
-                name,
-                email,
-                password:hashPassword,
-                confirmpassword:hashPassword,
-                pancard,
-                phone
-            });
-            const newAccount=new AccountModel({
-                email,
-                accountBalance:1000,
-                savingBalance:0,
-                expenseBalance:0,
-                investmentBalance:0,
-            });
-            await newAccount.save();
-            await newUser.save();
-            return res.send('Added successfully');
-        }
-        catch(err){
-            console.log(err);
-            
-        }
-    }
-);
+router.post('/login',loginuser);
 
 
 export default router;
